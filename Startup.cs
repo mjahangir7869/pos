d3d3d9fc548f6e0b2fd437c2268cae07b0042d50
Muhammad Jahangir <62006275+mjahangir7869@users.noreply.github.com>
2022-10-29 14:00:50 +0500

    first code

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PosShopManagement.Startup))]
+namespace PosShopManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
